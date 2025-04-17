2029bc9978da15b49d094b45519a09eef0651603
NathnSong <nadiasong332@gmail.com>
2025-04-18 05:31:20 +0900

    git 설정  수정

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -5,38 +5,9 @@ allprojects {
     }
 }
 
-// val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-// rootProject.layout.buildDirectory.value(newBuildDir)
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e9fc20e (프로젝트 구조 수정)
-// subprojects {
-//     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-//     project.layout.buildDirectory.value(newSubprojectBuildDir)
-// }
-// subprojects {
-//     project.evaluationDependsOn(":app")
-// }
-<<<<<<< HEAD
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-<<<<<<< HEAD
-}
-=======
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-=======
->>>>>>> e9fc20e (프로젝트 구조 수정)
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-<<<<<<< HEAD
 
 fun loadEnvProperty(key: String): String {
     val envFile = rootProject.file(".env")
@@ -47,9 +18,3 @@ fun loadEnvProperty(key: String): String {
 }
 
 extra["GOOGLE_MAPS_API_KEY"] = loadEnvProperty("GOOGLE_MAPS_API_KEY")
->>>>>>> 55ebd0d (지도 기능 구현)
-=======
->>>>>>> e9fc20e (프로젝트 구조 수정)
-=======
-}
->>>>>>> 1213d29 (로그인 ui수정)
